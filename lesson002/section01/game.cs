fbdf1976ec5f6083f5676fa6104fb163df41a9cb
gembaf <s15g464@stu.kagawa-u.ac.jp>
2016-11-17 16:17:00 +0900

    section01の問題を修正

--- lesson002/section01/game.cs
+++ lesson002/section01/game.cs
@@ -6,10 +6,25 @@ namespace Section01
   {
     public static void Main()
     {
-      Soldier soldier = new Soldier(100, 10); // Soldierをインスタンス化
+      // Soldierをインスタンス化
+      Soldier s1 = new Soldier(100, 30);
+      Soldier s2 = new Soldier(200, 10);
 
-      Console.WriteLine(soldier.hp);     // hpの値を確認
-      Console.WriteLine(soldier.power);  // powerの値を確認
+      WriteResult(s1, s2);
+
+      s2.hp -= s1.power; // s1がs2に攻撃
+      s1.hp -= s2.power; // s2がs1に攻撃
+
+      WriteResult(s1, s2);
+    }
+
+    public static void WriteResult(Soldier s1, Soldier s2)
+    {
+      // 値の出力
+      Console.WriteLine("---s1---");
+      Console.WriteLine("hp : " + s1.hp + ", power : " + s1.power);
+      Console.WriteLine("---s2---");
+      Console.WriteLine("hp : " + s2.hp + ", power : " + s2.power);
     }
   }
 }
