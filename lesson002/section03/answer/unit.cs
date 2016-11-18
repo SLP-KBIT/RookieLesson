8e44f60b60465e82731f21fb730b667fbdee0c94
gembaf <s15g464@stu.kagawa-u.ac.jp>
2016-11-18 19:13:15 +0900

    Section02に解答例を追加する

---
+++ lesson002/section03/answer/unit.cs
@@ -0,0 +1,32 @@
+namespace Section03.Answer
+{
+  abstract public class Unit
+  {
+    protected int hp;     // ヒットポイント
+    protected int power;  // 攻撃力
+
+    public Unit(int hp, int power)
+    {
+      this.hp = hp;
+      this.power = power;
+    }
+
+    public int getHp()
+    {
+      return hp;
+    }
+
+    public int getPower()
+    {
+      return power;
+    }
+
+    public void damage(int power)
+    {
+      hp -= power;
+    }
+
+    abstract public void powerUp();
+  }
+}
+
